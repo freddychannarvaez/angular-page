c13bb21632fadd4d70ee55084989e64a41aa4291
freddychannarvaez <freddy@routefusion.com>
2024-05-28 12:04:22 -0600

    Add basic content and assets

---
+++ src/environments/content.es.ts
@@ -0,0 +1,22 @@
+export const content = {
+  routes: [
+    {title: 'Nosotros', link: '/about-us', icon: 'people'},
+    {title: 'Productos', link: '/products', icon: 'work'},
+    {title: 'Contacto', link: '/contact', icon: 'contact_page'},
+  ],
+  main: {
+    description: `Welcome to Kinil Kab, your one-stop destination for all things honey! Whether you are a honey enthusiast, a health-conscious consumer, or simply someone with a sweet tooth, you'll find everything you need right here. Browse our wide selection of pure, raw honey varieties, each with its own unique flavor profile and nutritional benefits. From classic clover honey to exotic wildflower blends, we have something to satisfy every palate. Plus, explore our collection of beeswax candles, skincare products, and other bee-inspired goodies. Shop now and experience the pure bliss of Kinil Kab.`
+  },
+  about_us: {
+    description: `At Kinil Kab, we are dedicated to bringing you the purest, most delicious honey straight from our hives to your table. Nestled in the heart of pristine meadows, our beekeepers work tirelessly to ensure that every jar of honey is bursting with the goodness of nature. With a commitment to sustainability and ethical beekeeping practices, we strive to protect and nurture our precious pollinators while delivering unmatched quality to our customers.`
+  },
+  products: {
+    raw_honey: `Indulge in the pure goodness of our raw honey, harvested straight from the hive without any processing or additives. Choose from a variety of floral sources and taste the difference of 100% natural sweetness.`,
+    flavored_honey: `Elevate your taste experience with our selection of flavored honeys, infused with delicious ingredients like lavender, cinnamon, and ginger. Perfect for adding a touch of gourmet flair to your favorite dishes and beverages.`,
+    specialty_honey: `Explore our lineup of specialty honeys, including rare varietals like Manuka and Acacia, prized for their unique flavors and medicinal properties. Treat yourself to the luxury of premium honey sourced from the finest locations around the world.`,
+    end: `At Kinil Kab, we're passionate about bees, honey, and the incredible benefits they bring to our lives. Join us in celebrating the magic of the hive and experience the joy of pure, unadulterated honey like never before.`
+  },
+  social_networks: {
+    title: `Nuestras redes`
+  }
+}
