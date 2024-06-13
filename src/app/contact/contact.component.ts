03e281b5f9a27e708de185bf0becb6180edc1782
freddychannarvaez <freddy.chan.narvaez@gmail.com>
2024-06-13 17:42:29 -0600

    Add contact style

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  contactForm: any;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      phone: ['', Validators.required],
+      city: [''],
+      comment: ['', Validators.required]
+    });
+  }
+
+  onSubmitForm() {
+    console.log(this.contactForm.value);
+  }
 
 }
