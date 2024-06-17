5011af2d262e7c29a8514a35447b20e4780aac65
freddychannarvaez <freddy.chan.narvaez@gmail.com>
2024-06-17 12:42:31 -0600

    Add services basic design

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class ServicesComponent {
 
+  services: Service[] = [
+    {
+      title: 'Servicio 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      image: '../../assets/abb.png'
+    },
+    {
+      title: 'Servicio 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      image: '../../assets/delta.png'
+    },
+    {
+      title: 'Servicio 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      image: '../../assets/omron.png'
+    },
+    {
+      title: 'Servicio 4',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      image: '../../assets/sew2.png'
+    }
+  ]
+}
+
+
+interface Service {
+  title: string;
+  description: string;
+  image: string;
 }
