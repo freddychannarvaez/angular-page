f0aa7f18f4fee11d84ca99b787d7aedfdcb5d238
freddychannarvaez <freddy.chan.narvaez@gmail.com>
2024-07-04 22:54:56 -0600

    Improve style

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
+  clients: Client[] = [
+    {title: 'Bachoco', description: '', logo: '../../assets/bachoco.png'},
+    {title: 'Bepensa Motriz', description: '', logo: '../../assets/bepensa motriz.png'},
+    {title: 'Dunosusa', description: '', logo: '../../assets/dunosusa.png'},
+    {title: 'Glacial', description: '', logo: '../../assets/glacial.png'},
+    {title: 'Matpreo', description: '', logo: '../../assets/matpreo.png'},
+    {title: 'Predeco', description: '', logo: '../../assets/predeco.png'},
+    {title: 'Vertical', description: '', logo: '../../assets/vertical.png'},
+  ]
+}
 
+interface Client {
+  title: string;
+  description: string;
+  logo: string;
 }
--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -9,24 +9,28 @@ export class ServicesComponent {
 
   services: Service[] = [
     {
-      title: 'Servicio 1',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      image: '../../assets/abb.png'
+      title: 'Automatización de procesos',
+      image: '../../assets/AUTOMATIZACION.png'
     },
     {
-      title: 'Servicio 2',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      image: '../../assets/delta.png'
+      title: 'Cursos y capacitaciones',
+      image: '../../assets/CAPACITACION.png'
     },
     {
-      title: 'Servicio 3',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      image: '../../assets/omron.png'
+      title: 'Diseño industrial',
+      image: '../../assets/DISEÑO.png'
     },
     {
-      title: 'Servicio 4',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      image: '../../assets/sew2.png'
+      title: 'Venta de equipo eléctrico, instrumentación y control',
+      image: '../../assets/VENTA DE EQUIPO.png'
+    },
+    {
+      title: 'Manufactura de cilindros neumáticos',
+      image: '../../assets/FABRICACION.png'
+    },
+    {
+      title: 'Soporte técnico',
+      image: '../../assets/SOPORTE TECNICO.png'
     }
   ]
 }
@@ -34,7 +38,6 @@ export class ServicesComponent {
 const service1: Service[] = [
 {
   title: '',
-  description: `En MSA AUTOMATION DEVICES estamos enfocados en el crecimiento de los clientes por lo cual ofrecemos servicios de automatización, instrumentación y control, esto se logra con el esfuerzo de nuestro equipo multidisciplinario profesional en colaboración de los proveedores.`,
   image: ''
 }
 ];
@@ -42,6 +45,5 @@ const service1: Service[] = [
 
 interface Service {
   title: string;
-  description: string;
   image: string;
 }
